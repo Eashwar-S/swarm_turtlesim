e836072c1743c894e683fa329b2bc8a32d80db80
Eashwar-S <eashwar.sathyamurthy@gmail.com>
2025-02-23 17:57:20 -0500

    Need to debug the logic

---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'swarm_turtlesim'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='eashwar',
+    maintainer_email='eashwar@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'turtle_pattern_drawer = swarm_turtlesim.turtle_pattern_drawer:main'
+        ],
+    },
+)
